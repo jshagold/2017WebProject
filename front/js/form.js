c311cc08861e08646edf1c202606e600866d293e
Liunx <kdh950812@gmail.com>
2017-11-27 22:05:50 +0900

    불필요한 문장 지우

--- front/js/form.js
+++ front/js/form.js
@@ -9,13 +9,11 @@ function toggleAisde() {
 		$("article").stop().animate({
 			opacity: '1.0'
 		}, 500);
-		//$("article").unbind("mouseenter", toggleAisde);
 	}
 	else {
 		$("article").bind("click", toggleAisde);
 		$("article").stop().animate({
 			opacity: '0.5'
 		}, 500);
-		//$("article").bind("mouseenter", toggleAisde);
 	}
 }
